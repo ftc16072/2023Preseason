8121ca4d9ea1b162cba2bfaad5aa3036d8129e49
Alan Wong <alanwong9664@gmail.com>
2023-04-27 16:41:14 -0400

    not finished

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/QQTest/QQtest.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.ftc16072.QQTest;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public abstract class QQtest {
+    public final String name;
+
+    QQtest(String name) {
+        this.name = name;
+    }
+    public abstract void run(boolean on, Telemetry telemetry);
+
+
+
+}
