6f5c6e15244af232e5284ddf04d24f2898c505e8
Alan Wong <alanwong9664@gmail.com>
2023-06-14 16:09:41 -0400

    Functions created for claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Claw.java
@@ -1,14 +1,34 @@
 package org.firstinspires.ftc.teamcode.ftc16072.Mechanisms;
-
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.ftc16072.QQTest.QQtest;
-
 import java.util.List;
 
 public class Claw implements Mechanism{
+    public Servo claw;
+    private ColorRangeSensor sensor;
+    public static double GRIPPED_SERVO_POSITION = 0.15;
+    public static double RELEASED_SERVO_POSITION = 0.30;
+
+    public enum team_color {
+        blue,
+        red
+    }
+    private team_color Team_color;
     @Override
     public void init(HardwareMap hwMap) {
+        claw = hwMap.get(Servo.class,"claw");
+        sensor = hwMap.get(ColorRangeSensor.class,"cone_detector");
+        claw.setPosition(RELEASED_SERVO_POSITION);
+        if(sensor.blue()>sensor.red()){
+            Team_color = team_color.blue;
+        }else{
+            Team_color = team_color.red;
+        }
+
+
 
     }
 
@@ -23,12 +43,24 @@ public class Claw implements Mechanism{
     }
 
     public void manualClose(){
-
+        claw.setPosition(GRIPPED_SERVO_POSITION);
     }
     public void manualOpen(){
+        claw.setPosition(RELEASED_SERVO_POSITION);
+    }
+    public void Autograb(){
+        // if inRange and correctColor return True
+        // then Close the claw
 
     }
-    public void Autograb(color){
+    public boolean inRange(){
+
+
+    }
+    public boolean correctColor(){
+        /*
+
+         */
 
     }
 
