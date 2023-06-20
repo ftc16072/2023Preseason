b4f0189d9b17585b02b4ffd8444655e3a5381fa1
mahin <mahinwaghray@gmail.com>
2023-06-19 20:29:04 -0400

    Claw and Autograb operational

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ftc16072/Mechanisms/Claw.java
@@ -3,6 +3,7 @@ import com.qualcomm.robotcore.hardware.ColorRangeSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.ftc16072.QQTest.QQtest;
 import java.util.List;
 
@@ -12,26 +13,32 @@ public class Claw implements Mechanism{
     public static double GRIPPED_SERVO_POSITION = 0.15;
     public static double RELEASED_SERVO_POSITION = 0.30;
 
-    public enum team_color {
+    public enum TeamColor {
         blue,
-        red
+        red,
+        unknown
     }
-    private team_color Team_color;
+    private TeamColor teamColor;
     @Override
     public void init(HardwareMap hwMap) {
         claw = hwMap.get(Servo.class,"claw");
         sensor = hwMap.get(ColorRangeSensor.class,"cone_detector");
         claw.setPosition(RELEASED_SERVO_POSITION);
+        teamColor = TeamColor.unknown;
+        while(teamColor.equals(TeamColor.unknown)){
         if(sensor.blue()>sensor.red()){
-            Team_color = team_color.blue;
-        }else{
-            Team_color = team_color.red;
+            teamColor = TeamColor.blue;
         }
+        else if(sensor.red()>sensor.blue()){
+            teamColor = TeamColor.red;
+        }
+        else{
+            teamColor = TeamColor.unknown;
+        }}
 
 
 
     }
-
     @Override
     public List<QQtest> getTests() {
         return null;
@@ -48,30 +55,17 @@ public class Claw implements Mechanism{
     public void manualOpen(){
         claw.setPosition(RELEASED_SERVO_POSITION);
     }
-    public void Autograb(){
-        // if inRange and correctColor return True
-        // then Close the claw
-
-    }
     public boolean inRange(){
-
-
+        return sensor.getDistance(DistanceUnit.INCH) <= 3;
     }
-    public boolean correctColor(){
-        /*
-
-         */
-
-    }
-
-    /*
-
-    Manual Close
-    Manual Open
-    Autograb
-     - Checks color
-     - checks range
-     - after grab move lift
-
-     */
-}
+    public boolean CorrectColor() {
+        if ((teamColor.equals(TeamColor.blue)) && (sensor.blue() > sensor.red())) {
+            return true;
+        }
+        return teamColor.equals(TeamColor.red) && (sensor.blue() < sensor.red());
+     }
+    public void Autograb() {
+        if (CorrectColor() && inRange()) {
+            claw.setPosition(GRIPPED_SERVO_POSITION);
+        }
+    }}
